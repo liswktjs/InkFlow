3e6ccde1311601003ba69e7032be251e0a5be078
이자선 <liswktjs@gmail.com>
2025-01-06 16:10:39 +0900

    feat: drawer와 custom header 적용

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,40 @@
-import { Stack } from 'expo-router';
 import QueryProvider from '../src/provider/QueryProvider';
+import CustomHeader from '@/src/components/CustomHeader';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
+import { colors } from '@/src/style/color';
 
 export default function RootLayout() {
   return (
-    <QueryProvider>
-      <Stack />
-    </QueryProvider>
+    <GestureHandlerRootView>
+      <QueryProvider>
+        <Drawer
+          screenOptions={{
+            drawerStyle: {
+              backgroundColor: colors.background,
+            },
+            drawerLabelStyle: {
+              fontSize: 16,
+            },
+            drawerActiveTintColor: colors.background,
+            drawerActiveBackgroundColor: colors.accent,
+          }}
+        >
+          <Drawer.Screen
+            name="index"
+            options={{
+              header: CustomHeader,
+              title: '홈',
+            }}
+          />
+          <Drawer.Screen
+            name="select-subject"
+            options={{
+              title: '주제 고르기',
+            }}
+          />
+        </Drawer>
+      </QueryProvider>
+    </GestureHandlerRootView>
   );
 }
