4e63bf0b0506170c6eedf7d92784bd030339f3e7
이자선 <liswktjs@gmail.com>
2025-02-01 15:09:34 +0900

    feat: 문장 쓰기 성공 페이지 구현

---
+++ app/write/[id]/done.tsx
@@ -0,0 +1,12 @@
+import WriteDoneCTA from '@/src/pages/WriteDone/components/WriteDoneCTA';
+import { SafeAreaView } from 'react-native';
+
+const WriteSentenceDonePage = () => {
+  return (
+    <SafeAreaView>
+      <WriteDoneCTA />
+    </SafeAreaView>
+  );
+};
+
+export default WriteSentenceDonePage;
