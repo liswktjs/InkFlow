1cfdee520e12d060fe6720aed5027de92dc73bac
이자선 <liswktjs@gmail.com>
2025-02-02 21:15:34 +0900

    feat: 기록된 문장의 저자 혹은 내용을 수정할 수 있는 patch hook

---
+++ src/api/sentence/patch/usePatchSentencItem.ts
@@ -0,0 +1,53 @@
+import { useMutation, UseMutationResult } from '@tanstack/react-query';
+import { API_HOST } from '../../constant';
+import { SentenceItemType } from '../type';
+import { MutationOptionsType } from '../../type/queryUtilType';
+
+export interface PatchSentenceItemRequestType {
+  id: string;
+  author?: string;
+  content?: string;
+}
+
+const patchSentenceItem = async ({
+  id,
+  author,
+  content,
+}: PatchSentenceItemRequestType): Promise<SentenceItemType> => {
+  const response = await fetch(`${API_HOST}/${id}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      author,
+      content,
+    }),
+  });
+
+  return response.json();
+};
+
+interface Props {
+  options?: MutationOptionsType<
+    SentenceItemType,
+    Error,
+    PatchSentenceItemRequestType,
+    unknown
+  >;
+}
+
+const usePatchSentenceItem = ({
+  options,
+}: Props): UseMutationResult<
+  SentenceItemType,
+  Error,
+  PatchSentenceItemRequestType,
+  unknown
+> => {
+  return useMutation({
+    mutationFn: patchSentenceItem,
+    ...options,
+  });
+};
+
+export default usePatchSentenceItem;
