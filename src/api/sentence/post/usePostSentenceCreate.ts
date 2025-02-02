b6dafc52762269a3a7618654e45cb0af9c44cc41
이자선 <liswktjs@gmail.com>
2025-02-02 21:15:57 +0900

    feat: 입력할 수 있는 문장 추가 하는 post hook 구현

---
+++ src/api/sentence/post/usePostSentenceCreate.ts
@@ -0,0 +1,55 @@
+import { useMutation, UseMutationResult } from '@tanstack/react-query';
+import { API_HOST } from '../../constant';
+import { MutationOptionsType } from '../../type/queryUtilType';
+import { SentenceItemType } from '../type';
+
+export interface PostSentenceCreateRequestType {
+  author: string;
+  libraryId: string;
+  content: string;
+}
+
+const postSentenceCreate = async ({
+  author,
+  libraryId,
+  content,
+}: PostSentenceCreateRequestType): Promise<SentenceItemType> => {
+  const response = await fetch(`${API_HOST}/sentences`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      author,
+      libraryId,
+      content,
+    }),
+  });
+
+  return response.json();
+};
+
+interface Props {
+  options?: MutationOptionsType<
+    SentenceItemType,
+    Error,
+    PostSentenceCreateRequestType,
+    unknown
+  >;
+}
+
+const usePostSentenceCreate = ({
+  options,
+}: Props): UseMutationResult<
+  SentenceItemType,
+  Error,
+  PostSentenceCreateRequestType,
+  unknown
+> => {
+  return useMutation({
+    mutationFn: postSentenceCreate,
+    ...options,
+  });
+};
+
+export default usePostSentenceCreate;
